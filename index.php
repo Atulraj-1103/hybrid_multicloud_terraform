5523e1f888d2cee1135112baaa0ea235508e667c
Atulraj-1103 <60510032+Atulraj-1103@users.noreply.github.com>
2020-06-16 16:02:33 +0530

    first..

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ print "Hi welcome to the project\n";
 print `ifconfig`;
 print `free -m`;
 ?>
-<img src="http://d15buncx40kmfv.cloudfront.net/nature.jpg" height="600" width="400">
+<img src="http://d3ed55q14ur0qo.cloudfront.net/nature.jpg" height="600" width="400">
 </pre>
